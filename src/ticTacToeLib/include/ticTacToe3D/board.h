a50fa8e5d099cf2ccfbbb16dbaa0bf9e4ac99b3d
Bent Gramdal <bent@gramdal.no>
2022-12-30 07:01:20 +0100

    googletest json

--- src/ticTacToeLib/include/ticTacToe3D/board.h
+++ src/ticTacToeLib/include/ticTacToe3D/board.h
@@ -2,10 +2,12 @@
 
 #include "ticTacToe3D/lib.h"
 #include <stdint.h>
+#include <string>
 
 namespace ttt {
     struct TTT_API Board {
         Board();
         uint64_t state;
+        std::string ToStr() const;
     };
 }
